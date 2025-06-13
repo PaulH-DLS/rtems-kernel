09cd1536b5f3d4b0351e58e2bfeed4ae7a7568e5
Sepehr Ganji <sepehrganji79@gmail.com>
2025-06-13 15:32:37 -0600

    libfs/fatfs: Add initialization logic

--- cpukit/libfs/src/fatfs/rtems-diskio.c
+++ cpukit/libfs/src/fatfs/rtems-diskio.c
@@ -1,8 +1,61 @@
+#include <rtems.h>
+#include <rtems/libio.h>
+#include <rtems/diskdevs.h>
+#include <rtems/bdbuf.h>
+#include <rtems/error.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
 #include "diskio.h"
 #include "ff.h"
 
+#define MAX_DRIVES 4
+
+typedef struct
+{
+    bool initialized;
+    bool present;
+    bool write_protected;
+    char device_path[64];
+    rtems_disk_device *dd;
+    int fd;
+} drive_info_t;
+
+static drive_info_t drives[MAX_DRIVES];
+static bool diskio_initialized = false;
+
 DSTATUS disk_initialize(BYTE pdrv)
 {
+    DSTATUS status = STA_NOINIT;
+    rtems_status_code sc;
+
+    if (pdrv >= MAX_DRIVES)
+    {
+        return status;
+    }
+
+    if (!diskio_initialized)
+    {
+        memset(drives, 0, sizeof(drives));
+        diskio_initialized = true;
+        sc = rtems_disk_io_initialize();
+        if (sc != RTEMS_SUCCESSFUL)
+        {
+            return status;
+        }
+    }
+
+    if (drives[pdrv].initialized)
+    {
+        // TODO
+    }
+    // Map pdrv to a device instance
+
+    // Open or probe the block device using RTEMS IO layer
+
+    // Perform init step (sending CMD0/CMD1 for SD cards, etc.)
+
+    // Return RES_OK on success, or appropriate error (STA_NOINIT, STA_NODISK)
 }
 
 DSTATUS disk_status(BYTE pdrv)
